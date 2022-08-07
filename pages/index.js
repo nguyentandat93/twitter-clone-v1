914136ef1f6bed399cd303b2c5d2a27a601caefe
nguyen tan dat <ngutheky@gmail.com>
2022-08-08 00:22:27 +0700

    created feed component and its header

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Sidebar from "../components/Sidebar"
-
-
+import Feed from "../components/Feed"
 
 function HomePage() {
     return (
         <main className="flex min-h-screen max-w-7xl mx-auto">
+            {/* sidebar */}
             <Sidebar />
 
+            {/* feed */}
+            <Feed />
         </main>
     )
 }
